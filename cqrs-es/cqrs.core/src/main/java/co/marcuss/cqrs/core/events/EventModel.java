e7b2131c70c3a340662b2e649e91db2e3ddde052
Marcus S <m4rkuz@gmail.com>
2022-08-08 22:33:51 -0500

    Added EventModel

---
+++ cqrs-es/cqrs.core/src/main/java/co/marcuss/cqrs/core/events/EventModel.java
@@ -0,0 +1,28 @@
+package co.marcuss.cqrs.core.events;
+
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@Document(collation = "eventStore")
+public class EventModel {
+
+    @Id
+    private String id;
+
+    private LocalDateTime timeStamp;
+
+    private String aggregateIdentifier;
+
+    private String aggregateTye;
+
+    private int version;
+
+    private BaseEvent eventDate;
+
+}
