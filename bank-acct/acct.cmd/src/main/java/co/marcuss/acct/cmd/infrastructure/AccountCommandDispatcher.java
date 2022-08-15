db23c2cec4ce2ee660208c209ef8811df20c9d41
Marcus S <m4rkuz@gmail.com>
2022-08-14 20:37:32 -0500

    Minor fix

--- bank-acct/acct.cmd/src/main/java/co/marcuss/acct/cmd/infrastructure/AccountCommandDispatcher.java
+++ bank-acct/acct.cmd/src/main/java/co/marcuss/acct/cmd/infrastructure/AccountCommandDispatcher.java
@@ -24,8 +24,8 @@ public class AccountCommandDispatcher implements CommandDispatcher {
     @Override
     public void send(BaseCommand command) {
         var handlers = routes.get(command.getClass());
-        if (handlers.size() == 0) {
-
+        if (handlers == null || handlers.size() == 0) {
+            throw new RuntimeException("No command handler was registered!");
         }
         if (handlers.size() > 1) {
             throw new RuntimeException("We can not send the command to more than one handler");
